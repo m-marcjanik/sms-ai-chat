ab90165434d962e9b0b69dba58ad11a165f5ef10
m-marcjanik <michal@marcjanik.com>
2023-03-03 13:44:25 +0100

    Initial commit in develop branch.

--- chat.py
+++ chat.py
@@ -7,12 +7,13 @@ openai.api_key = os.environ.get("OPENAI_KEY")
 
 completion = openai.Completion()
 
-initial_info = "Human: We are roleplaying."
+initial_info = "System: We are two friends sending text messages to each other."
 
 incoming_number = input("Before starting chat, please state your username:\n")
 
 while True:
     incoming_msg = input(f"{incoming_number}: ")
+    model = "gpt-3.5-turbo"
 
     if os.path.exists(f"{incoming_number}.txt"):
         with open(f"{incoming_number}.txt", "r") as f:
@@ -21,13 +22,20 @@ while True:
         with open(f"{incoming_number}.txt", "w") as chat_log_content:
             chat_log_content.write(initial_info)
 
-    prompt = f'{chat_log_content}Human: {incoming_msg}\nAI:'
-    response = completion.create(prompt=prompt, engine="text-davinci-003", stop=['\nHuman'], temperature=0,
-                                 max_tokens=150)
-    answer = response.choices[0].text.strip()
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "We are roleplaying."},
+            {"role": "user", "content": f"{incoming_msg}"},
+        ],
+        temperature=0,
+    )
+    answer = response['choices'][0]['message']['content']
+
     print(f"AI: {answer}")
 
     log = open(f"{incoming_number}.txt", "a")
     log.write(f"Human: {incoming_msg}\nAI: {answer}\n")
     log.close()
+
 
